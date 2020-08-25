6f0dbbdf1b67cef4012f9986aa27faa9368d529c
jfcere <jfcere@sherweb.com>
2020-08-25 14:21:45 -0400

    Add Bootstrap customization

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bootstrap Customization
+
+Sample repository to share a Bootstrap customization across Angular and React applications.
+
+## Technology Stack
+
+- [Bootstrap](https://getbootstrap.com/) – Popular CSS Framework for developing responsive and mobile-first websites/applications
+- [Angular](https://angular.io/) – Development platform for building mobile/desktop web applications using TypeScript
+- [React](https://reactjs.org/) – A JavaScript library for building user interfaces
+- [Lerna](https://lerna.js.org/) – A tool for managing JavaScript projects with multiple packages
---
+++ bootstrap-lib/webpack.config.js
@@ -0,0 +1,56 @@
+const autoprefixer = require('autoprefixer');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = [{
+  entry: ['./index.scss', './index.js'],
+  output: {
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              }
+            },
+          },
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: 'package.json',
+        to: 'package.json',
+      },
+    ]),
+  ],
+}];
